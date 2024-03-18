b5aa0962f47237af89d1f6086334fb215e921b0a
imparcialista <lucasaraujoz13@outlook.com>
2024-03-18 01:08:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def main(page: ft.Page):
             payload = json.dumps({ "price": valor_atualizar })
 
         headers = {
-            "Access-Control-Allow-Origin": 'https://luarco.com.br',
+            "Access-Control-Allow-Origin": '*',
             "Authorization"              : f"Bearer {access_token_var}",
             "Content-Type"               : "application/json"
             }
